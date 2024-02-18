6e317e5ec525e313bdae6192ed889e8b6b369624
Lacy Morrow <me@lacymorrow.com>
2024-02-17 21:14:34 -0500

    scss

--- Florish/wp-content/themes/florish/404.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<div class="error cmn-gap text-center">
-    <div class="container">
-        <h2>404</h2>
-        <p>Ooops!!! The page you are looking for is not found</p>
-        <a href="#url">Back to Home</a>
-    </div>
-</div>
-<?php get_footer(); ?>
