8296d0d681e691fd457735f25846df620b3835cb
Lacy Morrow <me@lacymorrow.com>
2024-03-13 05:46:42 -0400

    fixing browsersync

--- web/wp-content/themes/florish/vite.config.ts
+++ web/wp-content/themes/florish/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	build: {
 		outDir: 'assets/dist', // Output directory
-		// emptyOutDir: true,
+		emptyOutDir: false,
 		copyPublicDir: false,
 		manifest: true,
 		minify: true,
@@ -12,20 +12,23 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: './assets/js/index.js', // Entry file where your JS/TS files are imported
-				// style: './assets/scss/style.scss', // TODO: Vite doesn't support scss sourcemaps, so we use sass to generate style.css
+				// style: './assets/scss/style.scss', // TODO: CSS COMPILATION IS DISABLED - Vite doesn't support scss sourcemaps, so we use sass to generate style.css
 			},
 			output: {
-				assetFileNames: '[name][extname]',
-				entryFileNames: '[name].js',
+				// Remove the file hashes!
+				// Todo: Maybe figure out how to load with hashes to prevent caches
+				// assetFileNames: '[name][extname]', // Sets style.css
+				entryFileNames: '[name].js', // main.js
 			},
 		},
 	},
+	// Todo: SCSS sourcemaps don't work, so browsersync reloads on scss changes (instead of HMR)
 	css: {
 		postcss: {
 			plugins: [
 				autoprefixer({}), // add options if needed
 			],
 		},
-		devSourcemap: true, // Todo: SCSS sourcemaps don't work, so browsersync reloads on scss changes (instead of HMR)
+		devSourcemap: true,
 	},
 });
